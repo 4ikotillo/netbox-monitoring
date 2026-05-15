8f5f76abb446c88245570c1548aa4444a2eaac8b
4ikotillo <58612823+4ikotillo@users.noreply.github.com>
2026-05-16 01:23:29 +0300

    v0.2

---
+++ netbox_monitoring/__init__.py
@@ -0,0 +1,19 @@
+from netbox.plugins import PluginConfig
+
+
+class NetBoxMonitoringConfig(PluginConfig):
+    name = "netbox_monitoring"
+    verbose_name = "NetBox Monitoring"
+    description = "Hosts + modems monitoring dashboard via Zabbix"
+    version = "0.2.0"
+    author = "4ikotillo"
+    base_url = "monitoring"
+    min_version = "4.0.0"
+    default_settings = {"zabbix_timeout": 10}
+
+    def ready(self):
+        super().ready()
+        from . import detectors  # noqa
+
+
+config = NetBoxMonitoringConfig
