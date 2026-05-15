45e57cbc4397a179d6f2dd6246135af0a8d70118
4ikotillo <58612823+4ikotillo@users.noreply.github.com>
2026-05-16 01:23:49 +0300

    v0.2

---
+++ netbox_monitoring/navigation.py
@@ -0,0 +1,13 @@
+from netbox.plugins.navigation import PluginMenu, PluginMenuItem
+
+menu = PluginMenu(
+    label="Monitoring",
+    groups=(
+        ("Overview", (
+            PluginMenuItem(link="plugins:netbox_monitoring:dashboard", link_text="Dashboard"),
+            PluginMenuItem(link="plugins:netbox_monitoring:group_add", link_text="New Group"),
+            PluginMenuItem(link="plugins:netbox_monitoring:zabbix_servers", link_text="Zabbix Servers"),
+        )),
+    ),
+    icon_class="mdi mdi-monitor-dashboard",
+)
