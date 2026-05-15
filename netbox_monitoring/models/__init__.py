a583a3183e61e1557d93ef2d64ba4bde80b8c9fb
4ikotillo <58612823+4ikotillo@users.noreply.github.com>
2026-05-16 01:25:33 +0300

    v0.2

---
+++ netbox_monitoring/models/__init__.py
@@ -0,0 +1,5 @@
+from .zabbix import ZabbixServer
+from .monitoring import MonitoredHost, MonitoredModem
+from .groups import MonitoringGroup
+
+__all__ = ["ZabbixServer", "MonitoredHost", "MonitoredModem", "MonitoringGroup"]
